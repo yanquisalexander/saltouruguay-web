990c4d945c2d3ebc7c1c3a906b9946bb0b6bb4d6
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-11-18 11:30:00 -0300

    Agregar soporte para el ícono de Discord en la lista de redes sociales

--- src/consts/SocialNetworks.ts
+++ src/consts/SocialNetworks.ts
@@ -2,6 +2,7 @@ import TwitchIcon from "@/icons/Twitch.astro";
 import InstagramIcon from "@/icons/Instagram.astro";
 import YouTubeIcon from "@/icons/YouTube.astro";
 import GitHubIcon from "@/icons/GitHub.astro";
+import DiscordIcon from "@/icons/Discord.astro";
 export const SOCIAL_NETWORKS = [
     {
         name: "Twitch",
@@ -17,6 +18,11 @@ export const SOCIAL_NETWORKS = [
         name: "YouTube",
         url: "https://www.youtube.com/channel/UCjDZNkQjIAHghGx-2cH6f0g",
         icon: YouTubeIcon,
+    },
+    {
+        name: 'Discord',
+        url: 'https://discord.gg/pdAk3QdqCK',
+        icon: DiscordIcon,
     },
     {
         name: "GitHub",
