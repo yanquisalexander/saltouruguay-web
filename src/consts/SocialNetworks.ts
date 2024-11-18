326aae7bbd186b52317da76d8ad7b67e4baf71b2
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-11-18 02:41:49 -0300

    Agregar componente de pie de página con enlaces a redes sociales y sus íconos

---
+++ src/consts/SocialNetworks.ts
@@ -0,0 +1,26 @@
+import TwitchIcon from "@/icons/Twitch.astro";
+import InstagramIcon from "@/icons/Instagram.astro";
+import YouTubeIcon from "@/icons/YouTube.astro";
+import GitHubIcon from "@/icons/GitHub.astro";
+export const SOCIAL_NETWORKS = [
+    {
+        name: "Twitch",
+        url: "https://twitch.tv/TheTangerineClub",
+        icon: TwitchIcon,
+    },
+    {
+        name: "Instagram",
+        url: "https://instagram.com/thetangerineclubofficial",
+        icon: InstagramIcon,
+    },
+    {
+        name: "YouTube",
+        url: "https://youtube.com/TheTangerineClub",
+        icon: YouTubeIcon,
+    },
+    {
+        name: "GitHub",
+        url: "https://github.com/yanquisalexander/TheTangerineClub",
+        icon: GitHubIcon,
+    }
+];
