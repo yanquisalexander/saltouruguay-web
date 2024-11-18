dd849bffe7bafc9ec795e962789ad2e0e4b27611
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-11-18 10:38:10 -0300

    Agregar animaciones de Tailwind y nueva página de premios

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import MiduAnimations from '@midudev/tailwind-animations'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -10,5 +12,7 @@ export default {
 			}
 		},
 	},
-	plugins: [],
+	plugins: [
+		MiduAnimations
+	],
 }
