9b2cb6c451faf6305aa1d0cf594c04a613c48567
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-11-20 12:23:00 -0300

    Agregar verificación de administrador y manejo de sesión en la página de administración

---
+++ src/lib/checkAdmin.ts
@@ -0,0 +1,15 @@
+import type { Session } from "@auth/core/types";
+
+// For Security (Discover secret pages), return 404 in each case
+export const checkAdmin = async (session: Session | null): Promise<Response | null> => {
+    if (!session) {
+        return new Response(null, { status: 404 });
+    }
+
+    if (!session.user.isAdmin) {
+        return new Response(null, { status: 404 });
+    }
+
+    return null; // Continue to the next handler
+
+}
