eb349c1c3d0ae415bc0dc2f99efef064193bfa0d
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-11-25 21:26:32 -0300

    Agregar nuevo logro "Campeón de liga" y eliminar importaciones no utilizadas en la página de comunidad

--- src/consts/Achievements.ts
+++ src/consts/Achievements.ts
@@ -1,6 +1,7 @@
 export const ACHIEVEMENTS = {
     CREATED_MEMBER_CARD: 'created-member-card',
     I_WAS_THERE_III: 'i-was-there-iii',
+    LEAGUE_WON: 'league-won',
 } as const
 
 export const ACHIEVEMENTS_TEXTS = [
@@ -13,5 +14,10 @@ export const ACHIEVEMENTS_TEXTS = [
         id: ACHIEVEMENTS.I_WAS_THERE_III,
         title: 'Yo estuve ahí III',
         description: 'Fuiste nominado en la tercera edición de #SaltoAwards',
+    },
+    {
+        id: ACHIEVEMENTS.LEAGUE_WON,
+        title: 'Campeón de liga',
+        description: 'Has participado en una liga y has ganado',
     }
 ]
