d2cd320fb9c1271016fa07beedb2a692558bf4b4
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-12-19 21:17:35 -0300

    Agregar constante VOTES_CLOSE_TIMESTAMP y eliminar cálculo redundante en config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,8 @@ import { DateTime } from 'luxon';
 
 export const VOTES_OPEN_TIMESTAMP = 1733022000000;
 
+export const VOTES_CLOSE_TIMESTAMP = 1734666300;
+
 export const EVENT_TIMESTAMP = 1734737400000;
 
 export const SALTO_BROADCASTER_ID = '238809411'
@@ -14,10 +16,6 @@ export const PUSHER_KEY = "mieit3wxzvktfm7gbxeq"
 export const SALTO_DISCORD_GUILD_ID = '700185692826763264'
 
 
-const VOTES_CLOSE_TIMESTAMP = DateTime.fromMillis(EVENT_TIMESTAMP, { zone: 'America/Montevideo' })
-    .minus({ days: 1 })
-    .set({ hour: 19, minute: 0, second: 0, millisecond: 0 })
-    .toMillis();
 
 export const nowInUruguay = () => DateTime.now().setZone('America/Montevideo').toMillis();
 
