8b88f933eb10dc3b1350a47ce76452447f3e21da
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2025-02-17 00:06:01 -0300

    Agregar configuración de Pusher para notificar nuevas versiones al finalizar la construcción

--- src/hooks/notifyNewVersion.ts
+++ src/hooks/notifyNewVersion.ts
@@ -1,4 +1,8 @@
-import { pusher } from "../utils/pusher";
+import Pusher from 'pusher';
+const { PUSHER_APP_CLUSTER, PUSHER_APP_ID, PUSHER_APP_KEY, PUSHER_APP_SECRET } = import.meta.env;
+
+const host = /* import.meta.env.DEV ? 'localhost' :  */`soketi.saltouruguayserver.com`;
+
 
 export default function notifyNewVersion() {
     return {
@@ -6,6 +10,15 @@ export default function notifyNewVersion() {
         hooks: {
             "astro:build:done": async () => {
                 try {
+                    const pusher = new Pusher({
+                        host,
+                        port: "443",
+                        appId: PUSHER_APP_ID,
+                        key: PUSHER_APP_KEY,
+                        secret: PUSHER_APP_SECRET,
+                        cluster: PUSHER_APP_CLUSTER,
+                        useTLS: true
+                    });
                     await pusher.trigger("streamer-wars", "new-version", null);
                 } catch (error) {
 
