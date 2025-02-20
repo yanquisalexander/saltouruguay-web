6268bd52f1b10b26370a11a021fa49ea62ec522e
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2025-02-20 00:07:34 -0300

    Ajustar estilo del mensaje de eliminación de jugador para mejorar la legibilidad en pantallas pequeñas

--- src/components/streamer-wars/PlayerEliminated.tsx
+++ src/components/streamer-wars/PlayerEliminated.tsx
@@ -48,7 +48,7 @@ export const PlayerEliminated = ({ playerNumber, session }: { playerNumber: numb
                             a
                         </span>
                     </div>
-                    <p class="text-3xl font-teko pt-16 text-center">
+                    <p class="text-3xl font-teko pt-16 text-center max-w-[80%] text-white">
                         {Array.isArray(playerNumber)
                             ? playerNumber.includes(session?.user.streamerWarsPlayerNumber!)
                                 ? "¡Has sido eliminado!"
