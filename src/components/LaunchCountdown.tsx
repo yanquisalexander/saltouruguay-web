5d0c5389152d4da03f623199437e96f626d81b77
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2025-02-21 14:13:09 -0300

    Agregar componente CountdownStreamerWars y mejorar LaunchCountdown con estilos personalizables

--- src/components/LaunchCountdown.tsx
+++ src/components/LaunchCountdown.tsx
@@ -3,16 +3,21 @@ import { useState, useEffect } from "preact/hooks";
 
 interface CountdownProps {
     timestamp: number; // Timestamp en milisegundos
+    classNames?: {
+        container?: string;
+        timeDisplay?: string;
+    }
 }
 
-const TimeDisplay = ({ value, label }: { value: number, label: string }) => (
+const TimeDisplay = ({ value, label, timeStyles }: { value: number, label: string, timeStyles?: string }) => (
     <div className="text-center space-y-2 aspect-square">
-        <div className="text-4xl font-medium italic font-anton text-yellow-500 tabular-nums">{value.toString().padStart(2, "0")}</div>
+        <div class={`text-4xl font-medium italic font-anton text-yellow-500 tabular-nums ${timeStyles}`}
+        >{value.toString().padStart(2, "0")}</div>
         <div className="text-gray-400">{label}</div>
     </div>
 );
 
-const LaunchCountdown = ({ timestamp, ...props }: CountdownProps & h.JSX.HTMLAttributes<HTMLDivElement>) => {
+const LaunchCountdown = ({ timestamp, classNames, ...props }: CountdownProps & h.JSX.HTMLAttributes<HTMLDivElement>) => {
     const calculateTimeLeft = () => {
         const now = new Date().getTime();
         const difference = timestamp - now;
@@ -48,10 +53,10 @@ const LaunchCountdown = ({ timestamp, ...props }: CountdownProps & h.JSX.HTMLAtt
 
     return (
         <div className={`flex space-x-6 justify-center mt-8 ${props.class}`} {...props}>
-            <TimeDisplay value={timeLeft.days} label="Días" />
-            <TimeDisplay value={timeLeft.hours} label="Horas" />
-            <TimeDisplay value={timeLeft.minutes} label="Minutos" />
-            <TimeDisplay value={timeLeft.seconds} label="Segundos" />
+            <TimeDisplay value={timeLeft.days} label="Días" timeStyles={classNames?.timeDisplay || ""} />
+            <TimeDisplay value={timeLeft.hours} label="Horas" timeStyles={classNames?.timeDisplay || ""} />
+            <TimeDisplay value={timeLeft.minutes} label="Minutos" timeStyles={classNames?.timeDisplay || ""} />
+            <TimeDisplay value={timeLeft.seconds} label="Segundos" timeStyles={classNames?.timeDisplay || ""} />
         </div>
     );
 };
