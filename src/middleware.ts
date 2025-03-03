8fbff63bd82dc1f99c3387206fa039c38311f404
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2025-03-03 18:40:23 -0300

    Actualizar el middleware para manejar el modo de mantenimiento usando una variable de entorno y permitir el bypass mediante un parámetro de consulta

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,24 @@
 import type { MiddlewareHandler } from "astro";
 
 const middleware: MiddlewareHandler = async (context, next) => {
-    const MAINTENANCE_MODE = import.meta.env.PROD
+    const MAINTENANCE_MODE = import.meta.env.MAINTENANCE_MODE === 'true';
 
-    if (MAINTENANCE_MODE && context.request.url.endsWith('/500') === false) {
+    // Leer el query param de la URL
+    const url = new URL(context.request.url);
+    const bypassMaintenance = url.searchParams.get('bypassMaintenanceMode') === 'true';
+
+    // Si está en mantenimiento y no es /500 y no tiene el bypass, muestra la página de mantenimiento
+    if (MAINTENANCE_MODE && !bypassMaintenance && !url.pathname.endsWith('/500')) {
         context.locals.isMaintenanceMode = true;
-        // Set 503 Not Available
-        const page500 = await context.rewrite('/500');
-        return new Response(page500.body, { ...page500, status: 503 });
+
+        // Mostrar página de mantenimiento (503) y retornar el contenido
+        const maintenancePage = await context.rewrite('/500');
+        return new Response(maintenancePage.body, { ...maintenancePage, status: 503 });
     }
 
+    // Si tiene el query `bypassMaintenanceMode`, deja seguir como si no estuviera en mantenimiento
     return next();
 };
 
-// Export onRequest. This should NOT be a default export
+// Export onRequest correctamente
 export const onRequest = middleware;
