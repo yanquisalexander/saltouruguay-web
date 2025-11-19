c6de41fbb9ce7b262a0b4387592b371d485ceeeb
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2025-11-18 22:35:24 -0300

    feat: agregar función slugify para formatear cadenas de texto

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const slugify = (text: string) => {
+  return text
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, "-") // Reemplaza espacios por guiones
+    .replace(/[^\w\-]+/g, "") // Elimina caracteres no alfanuméricos
+    .replace(/\-\-+/g, "-") // Reemplaza múltiples guiones por uno solo
+}
