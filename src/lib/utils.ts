ee39be9b799237ce5d59520406f37ac9492e30c4
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2025-11-18 22:36:54 -0300

    feat: agregar función createPusher para inicializar Pusher con configuración de entorno

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import type Pusher from "pusher-js";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -14,4 +15,15 @@ export const slugify = (text: string) => {
     .replace(/\s+/g, "-") // Reemplaza espacios por guiones
     .replace(/[^\w\-]+/g, "") // Elimina caracteres no alfanuméricos
     .replace(/\-\-+/g, "-") // Reemplaza múltiples guiones por uno solo
+}
+
+export function createPusher(): Pusher {
+  const Pusher = require("pusher-js");
+
+  const pusher = new Pusher(import.meta.env.PUBLIC_PUSHER_APP_KEY, {
+    cluster: import.meta.env.PUBLIC_PUSHER_APP_CLUSTER,
+    authEndpoint: "/api/pusher/auth",
+  });
+
+  return pusher;
 }
