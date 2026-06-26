aa4ea070045dcaf7cf4b0ff740731ce2c9263444
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2026-06-25 22:55:09 -0300

    dev: Change drizzle driver

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,19 +1,25 @@
 import "dotenv/config";
 
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
-import { drizzle } from "drizzle-orm/node-postgres";
-import pg from "pg";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
 import * as schema from "./schema";
 
-const { Pool } = pg;
+const {
+    DB_HOST,
+    DB_PORT = "5432",
+    DB_USER,
+    DB_PASSWORD = "",
+    DB_NAME,
+    DB_SSL,
+} = process.env;
 
-export const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME,
-    ssl: process.env.DB_SSL === "true" ? true : false,
-});
+const sslMode = DB_SSL === "true" ? "require" : "disable";
 
-export const client = drizzle(pool, { schema });
+const DATABASE_URL =
+    process.env.DATABASE_URL ??
+    `postgresql://${encodeURIComponent(process.env.DB_USER!)}:${encodeURIComponent(process.env.DB_PASSWORD ?? "")}@${process.env.DB_HOST}:${process.env.DB_PORT ?? "5432"}/${process.env.DB_NAME}?sslmode=${process.env.DB_SSL === "true" ? "require" : "disable"}`;
+
+const sql = neon(DATABASE_URL);
+
+export const client = drizzle(sql, { schema });
